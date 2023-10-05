efd3815663260879eaa57d8979db4609f86cf4b2
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-10-05 10:14:33 +0530

    Update find_eval_exec_files.py

--- find_eval_exec_files.py
+++ find_eval_exec_files.py
@@ -1,35 +1,29 @@
-import os
-
 def find_eval_exec_files(path):
   """
-  `eval()` और `exec()` का उपयोग करने वाली सभी फ़ाइलों की एक सूची लौटाता है।
+  `eval()` और `exec()` का उपयोग करने वाली सभी फ़ाइलों की एक सूची लौटाता है,
+  जिसमें `.min.js` फ़ाइलें शामिल नहीं हैं।
 
   Args:
     path: खोजने के लिए शुरू करने के लिए पथ।
 
   Returns:
-    `eval()` और `exec()` का उपयोग करने वाली फ़ाइलों की एक सूची।
+    `eval()` और `exec()` का उपयोग करने वाली फ़ाइलों की एक सूची,
+    जिसमें `.min.js` फ़ाइलें शामिल नहीं हैं।
   """
 
   files = []
   for root, _, filenames in os.walk(path):
     for filename in filenames:
       file_path = os.path.join(root, filename)
-      with open(file_path) as f:
+
+      # Skip `.min.js` files.
+      if filename.endswith(".min.js"):
+        continue
+
+      with open(file_path, encoding="utf-8") as f:
         source = f.read()
 
       if "eval(" in source or "exec(" in source:
         files.append(file_path)
 
   return files
-
-
-def main():
-  path = os.getcwd()
-  files = find_eval_exec_files(path)
-  for file in files:
-    print(file)
-
-
-if __name__ == "__main__":
-  main()
