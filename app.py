ce1b0fdaea48345738bd9013225862626b2fa7c2
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-10-13 07:06:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 @app.route('/')
-var=eval()
+var=eval(input('enter your val: '))
 def home():
     return render_template('index.html')
 if __name__ == "__main__":
